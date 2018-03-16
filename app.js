711e19c1f864985f66d7dfbdff7c7d8dd85137ac
Felipe Silva <felipeferreirasilva@gmail.com>
2018-03-16 19:52:26 -0300

    Publish

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
-const express               = require('express'),
-      mongoose              = require('mongoose'),
-      bodyParser            = require('body-parser'),
-      methodOverride        = require('method-override'),
-      passport              = require('passport'),
-      LocalStrategy         = require('passport-local'),
-      User                  = require('./models/user'),
-      Routes                = require('./routes');
-      app                   = express();
+const express           = require('express'),
+      mongoose          = require('mongoose'),
+      bodyParser        = require('body-parser'),
+      methodOverride    = require('method-override'),
+      passport          = require('passport'),
+      LocalStrategy     = require('passport-local'),
+      User              = require('./models/user'),
+      Routes            = require('./routes'),
+      app               = express();
 
+// PROD DATABASE URL
 mongoose.connect('mongodb://nzfd:nzfd6079@mongo_nzfd:27017/nzfd');
+
+// TEST DATABASE URL
 // mongoose.connect('mongodb://localhost/nzfd');
 
+// FOLDER PUBLIC SETUP
+app.use(express.static('public'));
+
+// EJS SETUP
 app.set('view engine', 'ejs');
 
+// BODY PARSER SETUP
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+
+// METHOD OVERRIDE SETUP
 app.use(methodOverride('_method'));
+
+// EXPRESS SESSION SETUP
 app.use(require('express-session')({secret: 'nzfd', resave: false, saveUninitialized: false}));
+
+// PASSPORT SETUP
 app.use(passport.initialize());
 app.use(passport.session());
-
-// ROUTES
-app.use(Routes);
-
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// IMPORT ROUTES
+app.use(Routes);
+
+// SERVICE START
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
     console.log('Server listening on port %s', port);
--- routes.js
+++ routes.js
@@ -3,15 +3,17 @@ const route                  = require('express').Router(),
       User                   = require('./models/user'),
       passport               = require('passport');
 
-
+// INDEX
 route.get('/', function(req, res){
     res.render('index', {logged: req.isAuthenticated(), user: req.user});
 });
 
+// ABOUT
 route.get('/about', function(req, res){
     res.render('about', {logged: req.isAuthenticated(), user: req.user});
 });
 
+// LIST QUESTIONS
 route.get('/questions', function(req, res){
     var query = req.query.search;
     if(query){
@@ -33,6 +35,7 @@ route.get('/questions', function(req, res){
     }
 });
 
+// CREATE QUESTION
 route.post('/questions', isLoggedIn, function(req, res){
     var title = req.body.title;
     var answer = req.body.answer;
@@ -42,10 +45,12 @@ route.post('/questions', isLoggedIn, function(req, res){
     res.redirect('/questions/new');
 });
 
+// CREATE QUESTION FORM
 route.get('/questions/new', isLoggedIn, function(req, res){
     res.render('newquestion', {logged: req.isAuthenticated(), user: req.user});
 });
 
+// LIST NONAPPROVED QUESTIONS
 route.get('/questions/toapprove', isLoggedIn, function(req, res){
     if(req.user.role === 'editor' || req.user.role === 'admin'){
         Questions.find({approved: false}, function(err, data){
@@ -60,6 +65,7 @@ route.get('/questions/toapprove', isLoggedIn, function(req, res){
     }
 });
 
+// UPDATE QUESTION
 route.put('/questions/:id', isLoggedIn, function(req, res){
     var id = req.body.id;
     var title = req.body.title;
@@ -76,20 +82,7 @@ route.put('/questions/:id', isLoggedIn, function(req, res){
     });
 });
 
-route.delete('/questions/:id', isLoggedIn, function(req, res){
-    if(req.user.role === 'admin'){
-        Questions.findByIdAndRemove(req.params.id, function(err, data){
-            if(err){
-                console.log(err);
-            }else{
-                res.redirect('/questions/toapprove');
-            }
-        });
-    }else{
-        res.redirect('/');
-    }
-});
-
+// UPDATE QUESTION FORM
 route.get('/questions/:id/edit', isLoggedIn, function(req, res){
     if(req.user.role === 'editor' || req.user.role === 'admin'){
         Questions.findById(req.params.id, function(err, data){
@@ -104,6 +97,22 @@ route.get('/questions/:id/edit', isLoggedIn, function(req, res){
     }
 });
 
+// DELETE QUESTION
+route.delete('/questions/:id', isLoggedIn, function(req, res){
+    if(req.user.role === 'admin'){
+        Questions.findByIdAndRemove(req.params.id, function(err, data){
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect('/questions/toapprove');
+            }
+        });
+    }else{
+        res.redirect('/');
+    }
+});
+
+// APPROVE QUESTION
 route.get('/questions/:id/approve', isLoggedIn, function(req, res){
     if(req.user.role === 'editor' || req.user.role === 'admin'){
         Questions.findByIdAndUpdate(req.params.id, {approved: true}, function(err, data){
@@ -118,6 +127,7 @@ route.get('/questions/:id/approve', isLoggedIn, function(req, res){
     }
 });
 
+// DISAPPROVE QUESTION
 route.get('/questions/:id/disapprove', isLoggedIn, function(req, res){
     if(req.user.role === 'editor' || req.user.role === 'admin'){
         Questions.findByIdAndUpdate(req.params.id, {approved: false}, function(err, data){
@@ -132,10 +142,12 @@ route.get('/questions/:id/disapprove', isLoggedIn, function(req, res){
     }
 });
 
+// REGISTER FORM
 route.get('/register', function(req, res){
     res.render('register', {logged: req.isAuthenticated(), user: req.user});
 });
 
+// REGISTER 
 route.post('/register', function(req, res){
     var name = req.body.name;
     var email = req.body.email;
@@ -150,20 +162,65 @@ route.post('/register', function(req, res){
     });
 });
 
+// LOGIN FORM
 route.get('/login', function(req, res){
     res.render('login', {logged: req.isAuthenticated(), user: req.user});
 });
 
+// LOGIN
 route.post('/login', passport.authenticate('local',{
     successRedirect: '/',
     failureRedirect: '/login'
 }), function(req, res){});
 
+// LOGOUT
 route.get('/logout', isLoggedIn, function(req, res){
     req.logout();
     res.redirect('/');
 });
 
+// USER FORM
+route.get('/user', isLoggedIn, function(req, res){
+    if(req.user.role === 'admin'){
+        var username = req.query.search;
+        if(username){
+            User.findOne({username: username}, function(err, data){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.render('user', {logged: req.isAuthenticated(), data: data, user: req.user});
+                }
+            });
+        }else{
+            res.render('user', {logged: req.isAuthenticated(), data: false, user: req.user});
+        }
+    }else{
+        res.redirect('/')
+    }
+});
+
+// USER UPDATE
+route.put('/user', function(req, res){
+    if(req.user.role === 'admin'){
+        var id = req.body.id;
+        var name = req.body.name;
+        var email = req.body.email;
+        var role = req.body.role;
+        var user = {name: name, username: email, email: email, role:role};
+
+        User.findByIdAndUpdate(id, user, function(err, data){
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect('/user');
+            }
+        });
+    }else{
+        res.redirect('/')
+    }
+});
+
+// MIDDLEWARE TO VERIFY IF USER IS LOGGED
 function isLoggedIn(req, res, next) {
     if(req.isAuthenticated()){
         return next();
