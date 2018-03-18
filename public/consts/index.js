00c9d5d5ed16b9f065ba18f46a2f49ec9506e7b6
Felipe Silva <felipeferreirasilva@gmail.com>
2018-03-18 14:41:04 -0300

    Publish

--- public/consts/index.js
+++ public/consts/index.js
@@ -3,11 +3,11 @@ const Strings =
     noPermission: 'Você não tem permissão para acessar esta pagina.',
     questionUpdated: 'Pergunta atualizada com sucesso!',
     questionDeleted: 'Pergunta excluida com sucesso!',
-    signupFinished: 'Registro efetuado com sucesso!',
+    signupFinished: 'Registro de usuario efetuado com sucesso!',
     loggedOut: 'Você foi deslogado com sucesso!',
     userUpdated: 'Usuario atualizado com sucesso!',
     noQuestionsFound: 'Sua busca não retornou nenhum resultado, tente usar outro termo.',
-    questionCreateSuccessful: 'Pergunta inseria com sucesso, aguarde aprovação do moderador',
+    questionCreateSuccessful: 'Pergunta inserida com sucesso, aguarde aprovação do moderador',
     questionApproved: 'Questão aprovada e enviada para a área de Perguntas!',
     questionRepproved: 'Questão reprovada e enviada para a área de Perguntas Pendentes.'
 };
